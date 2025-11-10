e0e607a65eb7fd5308a743ce5908d54223c4ee2a
NadineJuliana <nadine.juliana.b@web.de>
2025-11-10 17:29:07 +0100

    add templatefunction for render function

---
+++ scripts/templates.js
@@ -0,0 +1,23 @@
+function getBookTemplate(indexBooklist){
+    return `
+     <div id="bookListRef${indexBooklist}" class="bookContainer">
+            <h2>${books[indexBooklist].name}</h2>
+            <img src="" alt="">
+            <p class="priceInfo">${books[indexBooklist].price}</p>
+            <section class="likesection">
+                ${books[indexBooklist].likes}
+                <button onclick="" class="likebutton"></button>
+            </section>
+            <section class="bookinformations">
+                <p>Author: ${books[indexBooklist].author}</p>
+                <p>Erscheinungsjahr: ${books[indexBooklist].publishedYear}</p>
+                <p>Genre: ${books[indexBooklist].genre}</p>
+            </section>
+            <section class="commentdepartment">
+                <h3>Kommentare:</h3>
+                ${books[indexBooklist].comments}
+                <input class="commentInput" type="text">
+                <button onclick="" class="sharebutton"></button>
+            </section>
+        </div> `
+}
