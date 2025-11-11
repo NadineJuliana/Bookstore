51addd84ecfe50b36274772f3b4ff28338df0931
NadineJuliana <nadine.juliana.b@web.de>
2025-11-11 21:40:26 +0100

    summ functions in js

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,3 @@
-// function renderBookList() {
-//   let contentBooklistRef = document.getElementById('bookListContainer');
-
-//   for (let indexBooklist = 0; indexBooklist < books.length; indexBooklist++) {
-
-//     contentBooklistRef.innerHTML += getBookTemplate(indexBooklist);
-//     renderBookComments(indexBooklist);
-//   }
-
-// }
-
-
-// function renderBookComments(indexBooklist) {
-//   let bookCommentsRef = document.getElementById(`commentRef${indexBooklist}`);
-
-//   for (let indexComments = 0; indexComments < books[indexBooklist].comments.length; indexComments++) {
-//     let commentsName = books[indexBooklist].comments[indexComments].name;
-//     let commentsComment = books[indexBooklist].comments[indexComments].comment;
-//     bookCommentsRef.innerHTML += getCommentsTemplate(commentsName, commentsComment);
-//   }
-// }
-
-
-// function highlightBooks() {
-//   if (condition) {
-
-//   }
-// }
-
-
 function renderBookList() {
   let contentBooklistRef = document.getElementById('bookListContainer');
 
@@ -41,5 +11,12 @@ function renderBookList() {
       let commentsComment = books[indexBooklist].comments[indexComments].comment;
       bookCommentsRef.innerHTML += getCommentsTemplate(commentsName, commentsComment);
     }
+  }
+}
+
+
+function highlightBooks() {
+  if (condition) {
+
   }
 }
