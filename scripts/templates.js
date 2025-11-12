fac15add8f01f300cfa81beaf74dee27d9051123
NadineJuliana <nadine.juliana.b@web.de>
2025-11-12 14:39:23 +0100

    changed template input for styling options

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,30 +2,34 @@ function getBookTemplate(indexBooklist) {
     return `
      <div id="bookListRef${indexBooklist}" class="bookContainer">
             <h2>${books[indexBooklist].name}</h2>
-            <img src="" alt="">
-            <p class="priceInfo">${books[indexBooklist].price}</p>
-            <section class="likesection">
-                ${books[indexBooklist].likes}
-                <button onclick="" class="likebutton"></button>
+            <img class="bookImages" src="assets/img/Die_sieben_Schwestern1.jpg" alt="">
+            <section class="highlightSection">
+            <p class="priceInfo">${books[indexBooklist].price} €</p>
+            <section class="likeSection">
+                <p>${books[indexBooklist].likes}</p>
+                <img onclick="" class="likebutton" src="assets/icons/001-lieblings.png" alt="Likebutton">
             </section>
-            <section class="bookinformations">
-                <p>Author: ${books[indexBooklist].author}</p>
-                <p>Erscheinungsjahr: ${books[indexBooklist].publishedYear}</p>
-                <p>Genre: ${books[indexBooklist].genre}</p>
             </section>
-            <section class="commentdepartment">
+            <section class="bookInformations">
+                <p><b>Author:</b> ${books[indexBooklist].author}</p>
+                <p><b>Year of Publication:</b> ${books[indexBooklist].publishedYear}</p>
+                <p><b>Genre:</b> ${books[indexBooklist].genre}</p>
+            </section>
             <h3>Kommentare:</h3>
+            <section class="commentDepartment">
             <table id="commentRef${indexBooklist}">
             </table>
+            </section>
+            <section class="inputSection">
             <input class="commentInput" type="text">
-                <button onclick="" class="sharebutton"></button>
+                <img onclick="" class="sharebutton" src="assets/icons/006-like-button.png" alt="Send Comment">
             </section>
         </div> `
 }
 
 function getCommentsTemplate(commentsName, commentsComment) {
     return `
-    <th>${commentsName}</th>
-    <tr>${commentsComment}</tr>         
+    <th>[${commentsName}]</th>
+    <td>${commentsComment}</td>         
   `
 }
