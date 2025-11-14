1453a76097085f0b25983030ba0c2f3dd0a3757a
NadineJuliana <nadine.juliana.b@web.de>
2025-11-14 14:03:27 +0100

    add highlight(like) and add comment function

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,50 @@
 function renderBookList() {
   let contentBooklistRef = document.getElementById('bookListContainer');
-
+  contentBooklistRef.innerHTML = "";
   for (let indexBooklist = 0; indexBooklist < books.length; indexBooklist++) {
     contentBooklistRef.innerHTML += getBookTemplate(indexBooklist);
-
     let bookCommentsRef = document.getElementById(`commentRef${indexBooklist}`);
-
     for (let indexComments = 0; indexComments < books[indexBooklist].comments.length; indexComments++) {
       let commentsName = books[indexBooklist].comments[indexComments].name;
       let commentsComment = books[indexBooklist].comments[indexComments].comment;
       bookCommentsRef.innerHTML += getCommentsTemplate(commentsName, commentsComment);
     }
+    highlightedBook(indexBooklist);
   }
 }
 
 
-function highlightBooks() {
-  if (condition) {
 
+function highlightedBook(indexBooklist) {
+  let highlightImage = document.getElementById(`bookHighlight${indexBooklist}`);
+  let indexHighlight = document.getElementById(`counterLikes${indexBooklist}`);
+  if (books[indexBooklist].liked) {
+    highlightImage.src = "assets/icons/002-lieblings-1.png";
+    indexHighlight.innerHTML = "";
+    indexHighlight.innerHTML += `${books[indexBooklist].likes}`;
+  } if (!books[indexBooklist].liked) {
+    indexHighlight.innerHTML = "";
+    indexHighlight.innerHTML += `${books[indexBooklist].likes}`;
   }
+}
+
+function changeHighlight(indexBooklist) {
+  let highlightImage = document.getElementById(`bookHighlight${indexBooklist}`);
+  if (books[indexBooklist].liked) {
+    books[indexBooklist].likes--;
+    books[indexBooklist].liked = false;
+  } else {
+    highlightImage.src = "assets/icons/002-lieblings-1.png";
+    books[indexBooklist].likes++;
+    books[indexBooklist].liked = true;
+  }
+  renderBookList();
+}
+
+function addComment(indexBooklist) {
+  let newCommentRef = document.getElementById(`newComment${indexBooklist}`);
+  newComment = newCommentRef.value;
+  document.getElementById(`commentRef${indexBooklist}`).innerHTML += getAddCommentsTemplate(newComment);
+  books[indexBooklist].comments.push(newComment);
+  newCommentRef.value = "";
 }
